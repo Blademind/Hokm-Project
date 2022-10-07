4d396a7a038a7128fde7cc256cf3345d29d71cbe
eladyesh <eladyesh24@gmail.com>
2022-10-07 17:08:46 +0300

    added GameHandler functions

---
+++ UI/Hokm/GameHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokm
+{
+    public class GameHandler
+    {
+		public void GameLoop()
+		{
+			/* Runs the game loop */
+		}
+		public void PlayTurn()
+		{
+			/* main algorythmics function */
+		}
+		public void SendCard()
+		{
+			/* create a card and send
+			 * includes SetSuit */
+		}
+		public List<List<string>> GetDeck()
+        {
+			/* Deck looks like [[rank, suit], [rank, suit],....] */
+			return new List<List<string>>() { new List<string>() };
+        }
+	}
+}
---
+++ UI/Hokm/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hokm;
+
+namespace Hokm
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Client cli0 = new Client(new IPAddress(new byte[4] { 192, 168, 1, 196 }), 50000);
+            Client cli1 = new Client(new IPAddress(new byte[4] { 192, 168, 1, 196 }), 1234);
+            Client cli2 = new Client(new IPAddress(new byte[4] { 192, 168, 1, 196 }), 1235);
+            Client cli3 = new Client(new IPAddress(new byte[4] { 192, 168, 1, 196 }), 1236);
+            Client cli4 = new Client(new IPAddress(new byte[4] { 192, 168, 1, 196 }), 1237);
+
+
+
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Game());
+        }
+    }
+}
