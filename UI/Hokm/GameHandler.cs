05a2ddfefee0f784207c15a532e76c0e20d45f4c
Blademind <alonlevy2005@gmail.com>
2022-10-08 12:58:09 +0300

    added suits sending

--- UI/Hokm/GameHandler.cs
+++ UI/Hokm/GameHandler.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Net;
 using System.Text;
 
 namespace Hokm
 {
-    public class GameHandler
+    public partial class Client
     {
-		public void GameLoop()
+
+        public dynamic GameMessageParser(string msg)
+        {
+            if (Char.IsUpper(msg[0])) // card deck
+            {
+                deck = new List<List<string>>();
+                string[] cards = msg.Split("|");
+
+                foreach (string card in cards)
+                {
+                    string[] cardShapeSuit = card.Split("*");
+                    List<string> shapeSuit = new List<string>();
+                    foreach (string c in cardShapeSuit)
+                    {
+                        shapeSuit.Add(c);
+                    }
+                    deck.Add(shapeSuit);
+                }
+            }
+            else // ruler or client_id
+            {
+                string[] splitMessage = msg.Split(":");
+                if (splitMessage[0] == "ruler")
+                {
+                    ruler = Int32.Parse(splitMessage[1]);
+
+                    return "The ruler is: " + ruler;
+                }
+
+                else if (splitMessage[0] == "client_id")
+                {
+                    clientId = Int32.Parse(splitMessage[1]);
+
+                    return "Your client ID is: " + clientId;
+                }
+            }
+
+            return "";
+
+        }
+        public void GameLoop()
 		{
 			/* Runs the game loop */
 		}
@@ -19,6 +61,17 @@ namespace Hokm
 			/* create a card and send
 			 * includes SetSuit */
 		}
+        public void SendStrongSuit()
+        {
+            strong_suits = new string[4] { "SPADES", "CLUBS", "DIAMONDS", "HEARTS" };
+            Random rand = new Random();
+            Console.WriteLine("Sending strong suit...");
+            Console.WriteLine("set_strong:" + strong_suits[rand.Next(0, 4)]);
+            string msg_to_send = "set_strong:" + strong_suits[rand.Next(0, 4)];
+            Console.WriteLine(msg_to_send.Length.ToString("D8") + msg_to_send);
+            byte[] buffer = Encoding.ASCII.GetBytes(msg_to_send.Length.ToString("D8")+msg_to_send);
+            client_sock.Send(buffer);
+        }
 		public List<List<string>> GetDeck()
         {
 			/* Deck looks like [[rank, suit], [rank, suit],....] */
