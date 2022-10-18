6323616bd0f1588f5d2a48e894c27cc774228e6b
Blademind <alonlevy2005@gmail.com>
2022-10-18 23:16:03 +0300

    added some nice decorations

--- UI/Hokm/Program.cs
+++ UI/Hokm/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +21,26 @@ namespace Hokm
             //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
+            Console.WriteLine(@"
+  __   ___   ___  ___  ______       ___  ______  __   _ 
+ |. | /_  | |_  ||_  ||____  |     |_  ||____  | \ \ | |
+  | |   | |   | |  |_|     | |       | |     | |  \ \| |
+  | |___| | __| |     _____| |_      | |     | | __\ ` |
+  |_______||____|    /________/      | |     |_||______|
+                                     |_|                
+              ____  ___  __   __._______                
+             |__  ||_  | \ \ / /|.  __  |               
+                | |  |_|  \ V /  | |  | |               
+            ____| |     ___\  \  | | _| |               
+           /____/\_\   |______|  |_||___|               ");
+           Console.WriteLine(@"
+           .------..------..------..------.
+           |H.--. ||O.--. ||K.--. ||M.--. |
+           | :/\: || :/\: || :/\: || (\/) |
+           | (__) || :\/: || :\/: || :\/: |
+           | '--'H|| '--'O|| '--'K|| '--'M|
+           `------'`------'`------'`------'");
+            Thread.Sleep(2000);
             Client cli1 = new Client(new IPAddress(new byte[4] { 127, 0, 0, 1 }), 1234);
             //Client cli2 = new Client(new IPAddress(new byte[4] { 192, 168, 0, 176 }), 1235);
             //Client cli3 = new Client(new IPAddress(new byte[4] { 192, 168, 0, 176 }), 1236);
